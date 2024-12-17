94ca0073dcb7ff2f9cc9794bcf95491294c5b716
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2024-12-17 12:42:29 +0000

    Fix CSRF

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Gaby Dias v1.0"
+    return "Gaby Dias v2.0"
